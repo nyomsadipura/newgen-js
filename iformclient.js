0a7e828aa169cc756e84a15f731b35980f555022
dhanar rizky <130317220+dhanarrizky@users.noreply.github.com>
2024-07-05 10:43:16 +0700

    Update iformclient.js is signature button

--- iformclient.js
+++ iformclient.js
@@ -427,10 +427,10 @@ function actionDisableRejectReason() {
 }
 
 function isDigitalSignatureHideButton() {
-    const signatureVal = functions.getValue('DepSatTrxLeadsApplicant.isdigitalsignature');
+    const signatureVal = functions.getValue('IsDigitalSignatureFirst');
 
     functions.setStyle("button3", "disable",
-        (signatureVal !== true || signatureVal !== "true") ? "true" : "false");
+        signatureVal === true ? "true" : "false");
 
     functions.updateJSON();
 }
